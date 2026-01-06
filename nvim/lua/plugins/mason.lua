40e48835e808cbdb8a9478ed83b94798a993627f
Nacxht <zombiecrafter069@gmail.com>
2026-01-06 16:26:27 +0700

    feat: add denols

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright", "ts_ls" },
+                ensure_installed = { "lua_ls", "pyright", "ts_ls", "denols"},
             })
         end,
     },
@@ -31,6 +31,21 @@ return {
                 setup = {
                     capabilities = capabilities
                 },
+            })
+
+            -- Deno setup
+            vim.lsp.config("denols", {
+                setup = {
+                    capabilities = capabilities,
+                    root_dir = function(fname)
+                        local deno_root = vim.fs.root(0, { "deno.json", "deno.jsonc" })(fname)
+                        if deno_root then
+                            return nil
+                        end
+                        return vim.fs.root(0, "package.json")(fname)
+                    end,
+                    single_file_support = false
+                }
             })
 
             -- Run all config
